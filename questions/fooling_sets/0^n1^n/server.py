b384c6d299c9a306d465dd8ab71f6b1c888a43ae
jasonxia17 <jasonx3@illinois.edu>
2021-01-02 13:10:04 -0600

    Move shared html into question_base

--- questions/fooling_sets/0^n1^n/server.py
+++ questions/fooling_sets/0^n1^n/server.py
@@ -1,9 +1,11 @@
 import fooling_sets.server_base as server_base
 
 def generate(data):
-    server_base.generate(data)
-
     data["params"]["hints"] = [
         "Here is a helpful hint to get you started.",
-        "another useful hint"
+        "another useful hint",
+        "<a href=\"/\">link</a> $x^2$"
     ]
+
+    data["params"]["language_description"] = "$$L = \{0^n1^n \mid n \ge 0\}.$$"
+    server_base.generate(data)
--- serverFilesCourse/fooling_sets/server_base.py
+++ serverFilesCourse/fooling_sets/server_base.py
@@ -1,3 +1,5 @@
+import chevron
+
 def generate(data):
     data["params"]["names_for_user"] = []
 
@@ -5,3 +7,6 @@ def generate(data):
         {"name": "getFoolingSetElement", "type": "function"},
         {"name": "getDistinguishingSuffix", "type": "function"}
     ]
+
+    with open(data["options"]["server_files_course_path"] + "/fooling_sets/question_base.html") as f:
+        data["params"]["html"] = chevron.render(f, data).strip()
