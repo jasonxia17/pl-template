8aab6cf5f749f2e87c360baaba04eb42bb1c0bd4
jasonxia17 <jasonx3@illinois.edu>
2021-01-06 01:59:58 -0600

    Remove useless hints, add a legit hint

--- questions/fooling_sets/0^n1^n/server.py
+++ questions/fooling_sets/0^n1^n/server.py
@@ -2,9 +2,7 @@ import fooling_sets.server_base as server_base
 
 def generate(data):
     data["params"]["hints"] = [
-        "Here is a helpful hint to get you started.",
-        "another useful hint",
-        "<a href=\"/\">link</a> $x^2$"
+        "The strings in the fooling set should be prefixes of the strings in $L$."
     ]
 
     data["params"]["language_description"] = "$$L = \{0^n1^n \mid n \ge 0\}.$$"
